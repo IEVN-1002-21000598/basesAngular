7e3d3ca23ce0e09199ccc3ac186f8f45fc795342
Luis Flores <suarezflores33@gmail.com>
2024-09-18 20:56:34 -0600

    Primer commit Miercoles 18

---
+++ src/app/product/product-list/product-list.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrl: './product-list.component.css'
+})
+export class ProductListComponent {
+  productos:any[]=[
+  {
+    "productoID":1,
+    "Modelo":"Sentra",
+    "Descripcion":"4 puertas",
+    "Precio":30000,
+    "Year":2023,
+    "Marca":"NISSAN",
+    "Color":"Azul",
+    "imgenURL":"",
+  },
+  {
+    "productoID":2,
+    "Modelo":"A4",
+    "Descripcion":"4 puertas",
+    "Precio":20000,
+    "Year":2022,
+    "Marca":"AUDI",
+    "Color":"Blanco",
+    "imgenURL":"",
+  },
+  {
+    "productoID":3,
+    "Modelo":"Rio",
+    "Descripcion":"4 puertas",
+    "Precio":150000,
+    "Year":2020,
+    "Marca":"KIA",
+    "Color":"Azul",
+    "imgenURL":"",
+  },
+
+
+]
+
+}
